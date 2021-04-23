68d30e35c9636907b0c28a9ce1b8426da836d2e5
RobotCherries <zdip14@gmail.com>
2021-04-23 15:54:06 +0300

    Removed obsolete coffee-sort-type-map

--- src/app/core/maps/coffee-sort-type.map.ts
+++
@@ -1,8 +0,0 @@
-import { CoffeeSortByEnum } from '../enums/coffee-sort-type.enum';
-import { SortDirectionEnum } from '../enums/sort-direction.enum';
-
-export const CoffeeSortTypeMap: Map<CoffeeSortByEnum, SortDirectionEnum> = new Map([
-  [CoffeeSortByEnum.DateCreated, SortDirectionEnum.Descending],
-  [CoffeeSortByEnum.DateCreated, SortDirectionEnum.Ascending],
-  [CoffeeSortByEnum.Rating, SortDirectionEnum.Descending],
-]);
